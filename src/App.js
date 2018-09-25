59a182aa4e99ba15a3c43925cabd1ac70205f3c9
Diego <diego.slompo@sabion.com.br>
2018-09-25 17:53:35 -0300

    Feat - def UI icons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import * as Icon from 'react-icons/md'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+
+        <Icon.MdAcUnit />
+        <Icon.MdControlPoint />
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
